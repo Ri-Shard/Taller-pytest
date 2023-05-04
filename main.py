0a014862a066823c206a1d512fb97c058af884df
Rishard <rikelpa2001@gmail.com>
2023-05-04 09:56:35 -0500

    Add Fibonacci Fuction & Basic Test

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ def is_prime(num: int):
         if num % i == 0:
             return False
     return True
+
+@app.get("/fibonacci/{pos}")
+def fibonacci(pos: int):
+    if pos == 0:
+        return 0
+    elif pos == 1:
+        return 1
+    else:
+        a = 0
+        b = 1
+        for i in range(2, pos+1):
+            c = a + b
+            a = b
+            b = c
+        return b
--- test.py
+++ test.py
@@ -2,3 +2,6 @@ from main import *
 
 def test_isPrime_test():
     assert is_prime(1)==False
+    
+def test_fibonacci_test():
+    assert fibonacci(1)==1
