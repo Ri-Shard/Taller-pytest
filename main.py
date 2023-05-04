d567c8f4182d5100ba39fc9946735985b6cc71e0
Rishard <rikelpa2001@gmail.com>
2023-05-04 09:51:05 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from math import sqrt
+
+app = FastAPI()
+
+@app.get("/hello")
+def hello():
+    return "Hello FastAPI"
+
+@app.get("/IsPrime/{num}")
+def is_prime(num: int):
+    if num < 2:
+        return {"is_prime": False}
+    for i in range(2, int(sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
---
+++ test.py
@@ -0,0 +1,2 @@
+def test_isPrime_test():
+    assert is_primo(2)==True
