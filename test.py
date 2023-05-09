a9e9e752f406b00fd38caa7068bcecd62a9c6e39
Ri-Shard <rikelpa2001@gmail.com>
2023-05-09 11:34:15 -0500

    Test Endpoint Isprime false

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from main import *
+from fastapi.testclient import TestClient
+from main import app
 
+
+client = TestClient(app)
 def test_isPrime_test():
     assert is_prime(1)==False
 
 def test_fibonacci_test():
     assert fibonacci(8)==21
+
+def test_is_prime_endpoint():
+    response = client.get("/IsPrime/4")
+    assert response.status_code == 200
+    assert response.json() == False
